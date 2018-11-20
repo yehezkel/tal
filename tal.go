897dd7934e6d1a4ca34d783da4bdead252f69865
Amed Reyes <areyes@nerd.com>
2018-11-20 16:04:22 -0500

    Initial version

--- tal.go
+++ tal.go
@@ -1,116 +1,170 @@
 package tal
 
 import (
-	"time"
 	"errors"
 	"strconv"
+	"time"
 )
 
 var (
 	incompleteAnn = errors.New("Incomplete Annotation")
 	unexpectedEnd = errors.New("Unexpected end of tal")
+	invalidChar   = errors.New("Invalid Char")
 )
 
 const (
-	onSET = '\x15'
-	dURATION = '\x14'
-	end = '\x00'
-	onSET_PLUS = '+' 
-	onSET_MINUS = '-' 
+	onSET_PLUS  = '+'
+	onSET_MINUS = '-'
+
+	TOKEN_ONSET      = '\x15'
+	TOKEN_ANNOTATION = '\x14'
+	TOKEN_END        = '\x00'
 )
 
-
 type TimeStamp struct {
-	OnSet time.Duration
+	OnSet    time.Duration
 	Duration time.Duration
 }
 
-
 type Tal struct {
-	Stamp TimeStamp
+	Stamp      TimeStamp
 	Annotation []byte
 }
 
-func Parse(sammple []byte) ([]Tal, error) {
-	return []Tal{}, nil
+func Parse(sample []byte) ([]Tal, error) {
+
+    var result []Tal
+    i, l := 0, len(sample)
+
+    for  i < l {
+
+        stamp, j, err := parseStamp(sample)
+        if err != nil {
+            return result, err
+        }
+
+        i += j
+        for ;; {
+
+            ann, j, err := parseAnnotation(sample[i:])
+            if err != nil {
+                return result, err
+            }
+
+            i += j
+
+            result = append(result, Tal{
+                stamp,
+                ann,
+            })
+
+            if j > len(ann) + 1 {
+                break
+            }
+
+
+        }
+    }
+
+    return result, nil
 }
 
+func parseStamp(sample []byte) (TimeStamp, int, error) {
 
-func parseStamp(sammple []byte) (TimeStamp, int, error) {
-
-	l,i := len(sample), 0
+	i := 0
+	result := TimeStamp{}
 
 	if sample[i] != onSET_PLUS && sample[i] != onSET_MINUS {
-		//error
+		return result, 0, invalidChar
 	}
 
-	for i < l && sample[i] != onSET && sample[i] != dURATION{
-		
-		c := sample[i]
-		if c != 45 && c < 48 && c > 57 {
-			//error
-		}
-		i++
+	i, end := nextToken(sample)
+	if end {
+		return result, i, incompleteAnn
 	}
 
-	if i == l {
-		//error
+	token := sample[i]
+
+	if token == TOKEN_END {
+		return result, i, invalidChar
 	}
 
 	onSet, err := strconv.ParseFloat(string(sample[:i]), 64)
 	if err != nil {
-		//error
+		return result, i, err
 	}
 
-	if sample[i] == onSET {
+	result.OnSet = time.Duration(onSet * float64(time.Second))
 
-		for i < l && sample[i] != dURATION{
-		
-			c := sample[i]
-			if c != 45 && c < 48 && c > 57 {
-				//error
-			}
-			i++
+	//default value in case duration is not relevant
+	duration := 0.0
+	if token == TOKEN_ONSET {
+
+		i++
+		s := i
+
+		i, end = nextToken(sample[s:])
+		i = s + i
+		if end {
+			return result, i, incompleteAnn
 		}
 
-		if i == l {
-			//error
+		if sample[i] != TOKEN_ANNOTATION {
+			return result, i, invalidChar
 		}
 
-				
+		duration, err = strconv.ParseFloat(string(sample[s:i]), 64)
+		if err != nil {
+			return result, i, err
+		}
 
 	}
 
+	result.Duration = time.Duration(duration * float64(time.Second))
 
-
-
-
-	return TimeStamp{}, 0, nil
+	return result, i + 1, nil
 }
 
 //abc\x14dec\x14\x00
 //abc\x14\x00\x00
 func parseAnnotation(sample []byte) ([]byte, int, error) {
 
-	i, l := 0, len(sample)
-	for i < l &&  sample[i] != dURATION && sample[i] != end {
-		i++
+	l := len(sample)
+
+	pos, end := nextToken(sample)
+	if end {
+		return sample, pos, incompleteAnn
 	}
 
-	if i == l {
-		return sample, i, incompleteAnn
+	token := sample[pos]
+	ann := sample[:pos]
+
+	if token == TOKEN_END || token == TOKEN_ONSET {
+		return sample, pos, invalidChar
 	}
 
-	ann := sample[:i]
+	pos++
 
-	if sample[i] == dURATION {
-		i++
+	for pos < l && sample[pos] == TOKEN_END {
+		pos++
 	}
 
-	for i < l && sample[i] == end {
-		i++
-	}
-
-	return ann,i,nil 
+	return ann, pos, nil
 	//return []byte{},0,nil
+}
+
+func nextToken(input []byte) (pos int, end bool) {
+
+	l := len(input)
+
+	for pos < l &&
+		input[pos] != TOKEN_END &&
+		input[pos] != TOKEN_ANNOTATION &&
+		input[pos] != TOKEN_ONSET {
+
+		pos++
+	}
+
+	end = (pos == l)
+	return
 }
--- tal_test.go
+++ tal_test.go
@@ -2,6 +2,7 @@ package tal
 
 import (
 	"testing"
+    "time"
 )
 
 func TestParseAnnotationSimple(t *testing.T) {
@@ -17,7 +18,7 @@ func TestParseAnnotationSimple(t *testing.T) {
 
 	if string(ann) != "abcd" {
 		t.Errorf("Unexpected annotation: %s",string(ann))
-	} 
+	}
 }
 
 
@@ -33,28 +34,28 @@ func TestParseAnnotation(t *testing.T) {
 		iterations []Iteration
 	}{
 		{
-			"abc\x14cdb\x14\x00", 
+			"abc\x14cdb\x14\x00",
 			[]Iteration{
-				
+
 				Iteration{"abc",4},
 				Iteration{"cdb",5},
-				
+
 			},
 		},
 		{
-			"abc\x14\x14\x00", 
+			"abc\x14\x14\x00",
 			[]Iteration{
-				
+
 				Iteration{"abc",4},
 				Iteration{"",2},
-				
+
 			},
 		},
 	}
 
 	for _, current := range table {
 		input := []byte(current.input)
-		
+
 		for _,iteration := range current.iterations {
 			ann, count,err :=  parseAnnotation(input)
 			if err != nil {
@@ -68,8 +69,90 @@ func TestParseAnnotation(t *testing.T) {
 			if count != iteration.used {
 				t.Errorf("Wrong byte processed: %d, expecting %d",count, iteration.used)
 			}
-			
+
 			input = input[count:]
 		}
 	}
+}
+
+
+
+func TestParse(t *testing.T) {
+
+    type FlatTal struct {
+        onset string
+        duration string
+        annotation string
+    }
+
+    type Iteration struct {
+        input string
+        results []FlatTal
+    }
+
+
+    table := []Iteration{
+        Iteration{
+            "+120\x151\x14test\x14\x00",
+            []FlatTal{
+                FlatTal {"120s","1s","test"},
+            },
+        },
+
+        Iteration{
+            "+120\x14test\x14\x00",
+            []FlatTal{
+                FlatTal {"120s","0s","test"},
+            },
+        },
+
+        Iteration{
+            "+120\x14test\x14test2\x14\x00",
+            []FlatTal{
+                FlatTal {"120s","0s","test"},
+                FlatTal {"120s","0s","test2"},
+            },
+        },
+
+        Iteration{
+            "+120.3\x150.5\x14test\x14test2\x14\x00\x00",
+            []FlatTal{
+                FlatTal {"120.3s","0.5s","test"},
+                FlatTal {"120.3s","0.5s","test2"},
+            },
+        },
+    }
+
+    for _, current := range table {
+
+        list, err := Parse([]byte(current.input))
+        if err != nil {
+            t.Fatal(err)
+        }
+
+        if len(list) != len(current.results) {
+            t.Errorf("Unexpected results count: %d expecting %d", len(list),len(current.results))
+        }
+
+
+        for i, ann := range list {
+
+            rawExpected := current.results[i]
+            expOnset, _ := time.ParseDuration(rawExpected.onset)
+            expDuration, _ := time.ParseDuration(rawExpected.duration)
+
+            if ann.Stamp.OnSet != expOnset {
+                t.Errorf("Unexpected onset on %d got: %s expecting %s", i, ann.Stamp.OnSet,expOnset)
+            }
+
+            if ann.Stamp.Duration != expDuration {
+                t.Errorf("Unexpected duration on %d got: %s expecting %s", i, ann.Stamp.Duration,expDuration)
+            }
+
+
+            if rawExpected.annotation != string(ann.Annotation) {
+                t.Errorf("Unexpected annotation on %d got: %s expecting %s", i, ann.Annotation, rawExpected.annotation)
+            }
+        }
+    }
 }
